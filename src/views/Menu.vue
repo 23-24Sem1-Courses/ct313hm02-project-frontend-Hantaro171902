4b82d547a1805a5f4f32213401adde14d4debef5
Hantaro191702 <hanb2014913@student.ctu.edu.vn>
2023-11-17 22:18:43 +0700

    test connect

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,44 +1,44 @@
-<script setup> 
-import { ref, computed, onMounted, watch } from 'vue'; 
+<script setup>
+import { ref, computed, onMounted, watch } from 'vue';
 import { useRouter } from 'vue-router';
-import Menu from '@/components/Menu.vue';
-import Pagination from '@/components/Pagination.vue'; 
+// import Menu from '@/components/Menu.vue';
+// import Pagination from '@/components/Pagination.vue';
 
 const $router = useRouter();
-const totalPages = ref(1); 
-const currentPage =ref(1);
-const drinks = ref([]); 
-const selectedIndex =ref(-1); 
+const totalPages = ref(1);
+const currentPage = ref(1);
+const drinks = ref([]);
+const selectedIndex = ref(-1);
 const searchText = ref('');
 
 const searchableDrinks = computed(() =>
   drinks.value.map((drink) => {
-    const { dr_img, dr_name, dr_price, cate_id} = drink; 
+    const { dr_img, dr_name, dr_price, cate_id } = drink;
     return [dr_img, dr_name, dr_price, cate_id].join('');
   })
 );
 
 const filteredDrinks = computed(() => {
-  if (!searchText.value) 
-    return drinks.value; 
-    return drinks.value.filter((drink, index)=> 
-      searchableDrinks.value[index].includes(searchText.value)
+  if (!searchText.value) return drinks.value;
+  return drinks.value.filter((drink, index) =>
+    searchableDrinks.value[index].includes(searchText.value)
   );
 });
 
-const selectedDrink= computed(() => {
+const selectedDrink = computed(() => {
   if (selectedIndex.value < 0) return null;
-    return filteredDrinks.value[selectedIndex.value];
+  return filteredDrinks.value[selectedIndex.value];
 });
 
 async function retrieveDrinks(page) {
   try {
     const chunk = await drinksService.getDrinks(page);
-    totalPages.value = chunk.metadata.lastPage ?? 1; 
-    drinks.value = chunk.drinks.sort((current, next) => 
-      current.dr_name.localeCompare(next.dr_name));
-      selectedIndex.value = -1;
-  }catch (error) { 
+    totalPages.value = chunk.metadata.lastPage ?? 1;
+    drinks.value = chunk.drinks.sort((current, next) =>
+      current.dr_name.localeCompare(next.dr_name)
+    );
+    selectedIndex.value = -1;
+  } catch (error) {
     console.log(error);
   }
 }
@@ -49,10 +49,6 @@ watch(searchText, () => (selectedIndex.value = -1));
 
 watchEffect(() => retrieveDrinks(currentPage.value));
 </script>
-
-<template>
-
-</template>
 
 <script>
 export default {
@@ -68,6 +64,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
