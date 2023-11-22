77df37fb44a089391c8f4c520b7267c6386f8be2
Hantaro191702 <hanb2014913@student.ctu.edu.vn>
2023-11-22 20:34:33 +0700

    udate add product

--- src/views/MenuAdd.vue
+++ src/views/MenuAdd.vue
@@ -1,26 +1,37 @@
+<template>
+  <div class="page">
+    <ProductForm :initial-drink="drink" @submit:drink="onAdd" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
 <script setup>
 import { ref } from 'vue';
 
-import Menu from '@/components/Menu.vue';
+import ProductForm from '@/components/ProductForm.vue';
 import makeDrinksService from '@/services/drinks.service';
+import { useRouter } from 'vue-router';
 
-const message = ref('');
 const drinksService = makeDrinksService();
+const message = ref('');
+const router = useRouter();
 
 async function onAdd(drink) {
+  console.log('Starting onAdd function');
+  console.log('Received drink:', drink);
+
   try {
+    console.log('Calling drinksService.createDrink');
     await drinksService.createDrink(drink);
-    message.value = 'New product added successfully';
+    console.log('Sign up successful!');
+    message.value = 'Sign up successfully!';
+    setTimeout(() => {
+      // You can use router.push to navigate to another page
+      router.push('/menu');
+    }, 1000);
   } catch (error) {
-    console.log(error);
+    console.error('Error during sign up:', error);
     // Redirect to NotFound page and keep URL intact
   }
 }
 </script>
-<template>
-  <div class="page">
-    <h4>Add Drink</h4>
-    <Menu :initial-drink="drink" @submit:drink="onAdd" />
-    <p>{{ message }}</p>
-  </div>
-</template>
